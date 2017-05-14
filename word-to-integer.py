5f4667118bf3cbf7e34cef174c785e545641c6ba
Beth Tenorio <betsky98@gmail.com>
2017-05-14 15:05:40 -0400

    associate the encoded message with a secret key

--- word-to-integer.py
+++ word-to-integer.py
@@ -4,21 +4,32 @@ class TextPorter():
 
     def __init__(self):
         self.word_dictionary = {}
+        self.secret_codes = {}
+        self.secret = self.register_secret_code()
 
     def unique_id(self):
         limit = 141167095653376
         return random.randint(1, limit)
 
+    def register_secret_code(self):
+        secret = raw_input('Enter your secret key (max 10 characters): ')
+        self.secret_codes[secret] = []
+
+        return secret
+
     def encode(self):
         inputs = raw_input('Enter a series of words: ').split(" ")
 
         for i in filter(None, inputs):
+            word_id = self.unique_id()
             if len(i) <= 10:
-                self.word_dictionary[self.unique_id()] = i
+                self.word_dictionary[word_id] = i
+                self.secret_codes[self.secret].append(word_id)
             else:
                 print('The word {} exceeds the word limit and will not be encoded'.format(i))
 
-        return self.word_dictionary
+        print(self.word_dictionary)
+        return self.secret_codes
 
 
     def decode(self):

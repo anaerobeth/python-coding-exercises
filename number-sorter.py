da8532e6eb79c001fd9a418786ac08a8623bf3d6
Beth Tenorio <betsky98@gmail.com>
2017-05-15 10:31:18 -0400

    handle sorting of arrays with more than 3 elements

--- number-sorter.py
+++ number-sorter.py
@@ -1,20 +1,35 @@
 class SimpleSort():
+    """
+    Loop through indexes ranging from 1 to length of array:
+    set current to value at index
+    set pointer to index
+    compare current to value before the pointer(tail)
+    if tail is greater than current:
+        write the tail to array at pointer position
+        move the pointer backwards and repeat
+        stop when pointer reaches 0 or tail is less
+    if pointer is not the same as index:
+        write the current to the array at pointer position
+
+
+    TODO: how to swap positions in array?
+    """
+
 
     def sort(self):
-        array = [3,2,7]
-        temp = [array.pop()]
+        array = [3,2,7,5,1]
+        for i in range(1, len(array)):
+            current =  array[i]
+            pointer = i
 
-        for index, num in enumerate(temp):
-            if len(array) >= 1:
-                item = array.pop()
-            else:
-                break
-            if num > item:
-                temp.insert(index, item)
-            else:
-                temp.insert(index+1, item)
+            while((pointer > 0) and (array[pointer-1] > current)):
+                array[pointer] = array[pointer-1]
+                pointer = pointer -1
 
-        return temp
+            if pointer != i:
+                array[pointer] = current
+
+        return array
 
 s = SimpleSort()
 print(s.sort())

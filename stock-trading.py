41319be722d648f1719d8c499cde1fb3e9add571
Beth Tenorio <betsky98@gmail.com>
2017-06-23 23:30:27 -0400

    handle non-profitable trades

--- stock-trading.py
+++ stock-trading.py
@@ -13,25 +13,31 @@ def get_max_profit(stock_prices):
         min_price = stock_prices[0]
 
         # loop through prices
-        for current_price in stock_prices:
+        for index, current_price in enumerate(stock_prices):
 
-            # is this the lowest price seen?
-            min_price = min(min_price, current_price)
+            # don't allow buying and selling at time 0
+            if index == 0:
+                continue
+
+            # update max_profit if we can do better
             profit = current_price - min_price
-
-            # update profit if we can do better
             if profit > max_profit:
                 max_profit = profit
                 buy, sell = min_price, current_price
 
-    if buy == sell:
-        return 'No trade is possible today'
+            # is this the lowest price seen?
+            min_price = min(min_price, current_price)
+
+    if buy >= sell:
+        return 'No profitable trade was possible today'
     else:
         return (max_profit, buy, sell)
 
 
-assert get_max_profit([]) == 'No trade is possible today'
-assert get_max_profit([1, 1, 1, 1, 1]) == 'No trade is possible today'
+assert get_max_profit([]) == 'No profitable trade was possible today'
+assert get_max_profit([1, 1, 1, 1, 1]) == 'No profitable trade was possible today'
+assert get_max_profit([5, 1, 1, 1, 1]) == 'No profitable trade was possible today'
+assert get_max_profit([5, 5, 5, 1, 1]) == 'No profitable trade was possible today'
 
 sample = [10, 7, 5, 8, 11, 9, 4]
 assert get_max_profit(sample) == (6, 5, 11)

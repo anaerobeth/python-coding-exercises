ce7b9743e5ab9530e8bbab4123e47037b81a1119
Beth Tenorio <betsky98@gmail.com>
2017-06-23 22:10:31 -0400

    refactor get_max_profit
    loops through entire list with fewer steps in the inner loop but still O(n^2)

--- stock-trading.py
+++ stock-trading.py
@@ -10,16 +10,14 @@ def get_max_profit(stock_prices):
     if not stock_prices:
         pass
     else:
-        rev = stock_prices[::-1]
-        print(rev)
-        for index, k in enumerate(rev):
-            print("Selling at ", k)
-            for j in rev[index+1:]:
-                print("   Buying at ", j)
-                if k - j > max_profit:
-                    max_profit = k - j
-                    buy, sell = j, k
-                    print("     Profit of ", max_profit)
+        # loop through prices to buy at
+        for index, k in enumerate(stock_prices):
+            # loop through prices to sell at
+            for j in stock_prices[index+1:]:
+                if j - k > max_profit:
+                    # update profit if we can do better
+                    max_profit = j - k
+                    buy, sell = k, j
 
     if buy == sell:
         return 'No trade is possible today'
@@ -27,11 +25,11 @@ def get_max_profit(stock_prices):
         return (max_profit, buy, sell)
 
 
-# assert get_max_profit([]) == 'No trade is possible today'
-# assert get_max_profit([1, 1, 1, 1, 1]) == 'No trade is possible today'
+assert get_max_profit([]) == 'No trade is possible today'
+assert get_max_profit([1, 1, 1, 1, 1]) == 'No trade is possible today'
 
 sample = [10, 7, 5, 8, 11, 9, 4]
 assert get_max_profit(sample) == (6, 5, 11)
-# max_profit, buy, sell = get_max_profit(sample)
-# print("Buy at ${} and sell at ${} for a profit of ${}".format(buy, sell, max_profit))
+max_profit, buy, sell = get_max_profit(sample)
+print("Buy at ${} and sell at ${} for a profit of ${}".format(buy, sell, max_profit))
 

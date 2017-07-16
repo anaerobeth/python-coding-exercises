876db0e9282d20a90f83cd903f05e9bf52b3a2bb
Beth Tenorio <betsky98@gmail.com>
2017-07-15 23:17:10 -0400

    make retrieving min, max or mean more efficient
    O(1) lookup instead of O(n) by calculating on init or insert

--- temp-tracker.py
+++ temp-tracker.py
@@ -3,28 +3,30 @@ TempTracker
 Temperature in Fahrenheit, range 0..110
 """
 from collections import Counter
+import sys
 
 class TempTracker():
     values = []
+    num_values = 0
+    max_val = -sys.maxsize
+    min_val = sys.maxsize
+    total_values = 0
 
     def __init__(self, *args):
         for arg in args:
-            self.values.append(arg)
+            self.update_values(arg)
 
-    def insert(self, temp):
-        self.values.append(temp)
+    def insert(self, arg):
+        self.update_values(arg)
 
     def get_max(self):
-        return sorted(self.values)[-1]
+        return self.max_val
 
     def get_min(self):
-        return sorted(self.values)[0]
+        return self.min_val
 
     def get_mean(self):
-        total = 0
-        for value in self.values:
-            total += value
-        return total / len(self.values)
+        return self.total_values / self.num_values
 
     def get_mode(self):
         values_dict = {}
@@ -40,11 +42,21 @@ class TempTracker():
         sorted_values = sorted(values_dict.items(), key=lambda x: x[1])
         return sorted_values[-1][0]
 
+    def update_values(self, arg):
+        self.num_values += 1
+        self.values.append(arg)
+        if arg > self.max_val:
+            self.max_val = arg
+        if arg < self.min_val:
+            self.min_val = arg
+        self.total_values += arg
+
+
 
 t = TempTracker(12, 14, 20)
-print(t.values)
+print(t.num_values)
 t.insert(20)
-print(t.values)
+print(t.num_values)
 print(t.get_max())
 print(t.get_min())
 print(t.get_mean())

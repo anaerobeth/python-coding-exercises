cef86fa03606a0adfbfbda2da424b4abc338d990
Beth Tenorio <betsky98@gmail.com>
2017-07-17 23:58:36 -0400

    faster searching for inner loop using a set

--- movie-streaming.py
+++ movie-streaming.py
@@ -5,11 +5,12 @@ Choose two movies whose total runtimes will equal the exact flight length
 
 def choose_movies(flight_length, movie_lengths):
     match = False
+    seen_movies = set()
     for movie_length in movie_lengths:
-        for other_movie_length in movie_lengths:
-            if movie_length + other_movie_length == flight_length:
-                print(movie_length, other_movie_length)
-                match = True
+        other_movie_length = flight_length - movie_length
+        if other_movie_length in seen_movies:
+            match = True
+        seen_movies.add(movie_length)
     return match
 
 print(choose_movies(4, [1, 2, 3, 4]))

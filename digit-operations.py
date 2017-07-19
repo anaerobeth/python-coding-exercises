dfad5531645795963eff68db70861fc80dfa78b6
Beth Tenorio <betsky98@gmail.com>
2017-07-18 22:07:18 -0400

    manipulate digits of an integer

---
+++ digit-operations.py
@@ -0,0 +1,44 @@
+"""
+Operations involving digits of an integer
+"""
+
+import sys
+
+def sum_of_digits(number):
+    total = 0
+    for num in str(number):
+        total += int(num)
+
+    return total
+
+
+def product_of_digits(number):
+    product = 1
+    for num in str(number):
+        product *= int(num)
+
+    return product
+
+
+def largest_digit(number):
+    largest = -sys.maxsize
+    for num in str(number):
+        if int(num) > largest:
+            largest = int(num)
+
+    return largest
+
+
+def smallest_digit(number):
+    smallest = sys.maxsize
+    for num in str(number):
+        if int(num) < smallest:
+            smallest = int(num)
+
+    return smallest
+
+
+print(sum_of_digits(456))
+print(product_of_digits(456))
+print(largest_digit(456))
+print(smallest_digit(456))

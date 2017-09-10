ba225a93dc2a9f81ec1e9f96d3113f1dcb912291
Beth Tenorio <betsky98@gmail.com>
2017-09-09 22:50:13 -0400

    find the mode in a set of items

---
+++ mode.py
@@ -0,0 +1,29 @@
+"""
+Given a set of n items, determine the mode (element that occurs the most number of times)
+"""
+from mergesort import sorter
+
+def mode(given):
+    arr = sorter(given)
+    mode = arr[0]
+    longest_run = 0
+    runs = 0
+
+    for n in range(len(arr)-1):
+        if n < len(arr):
+            diff = abs(arr[n] - arr[n+1])
+
+            if diff == 0:
+                runs += 1
+                if longest_run < runs:
+                    longest_run = runs
+                    mode = arr[n]
+            else:
+                run = 0
+    return mode
+
+
+given = [5, 9, 1, 3, 4, 4, 8]
+print(mode(given))
+
+

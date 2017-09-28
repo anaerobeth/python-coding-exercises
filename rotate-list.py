60d07ff83ca2e8fd3b8cad65cb6ad5a2c4ccb43d
Beth Tenorio <betsky98@gmail.com>
2017-09-28 17:23:30 -0400

    handle exceptions if valid step is not provided

--- rotate-list.py
+++ rotate-list.py
@@ -1,17 +1,19 @@
-def rotate(arr, step):
+def rotate(arr, step=None):
     """ Return a list that has been rotated the required number of steps """
-    if type(step) == int:
-        if step == 0:
+    try:
+        if int(step) == 0:
             return arr
 
-        if step < 0:
+        if int(step) < 0:
             """ Rotating in reverse is the same as rotating the number of steps less than the length of the list"""
             step = len(arr) + step # step is negative
             print(step)
 
         return (arr + arr[:step])[step:]
-    else:
-        print('Invalid input')
+    except ValueError:
+        return 'Please provide a step'
+    except:
+        return 'Invalid input'
 
 
 
@@ -21,3 +23,6 @@ assert rotate(arr, 3) == [4, 5, 1, 2, 3]
 assert rotate(arr, 0) == [1, 2, 3, 4, 5]
 assert rotate(arr, -1) == [5, 1, 2, 3, 4]
 assert rotate(arr, -2) == [4, 5, 1, 2, 3]
+print(rotate(arr, 2.0))
+print(rotate(arr, 'foo'))
+print(rotate(arr))

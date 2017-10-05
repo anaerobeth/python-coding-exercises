3c75a25bab944c1e39207ebcde5bd461f2cdcd45
Beth Tenorio <betsky98@gmail.com>
2017-10-04 23:19:46 -0400

    print contents of a directory

---
+++ print-dir-contents.py
@@ -0,0 +1,12 @@
+import os
+
+def print_directory_contents(path):
+    for child in os.listdir(path):
+        child_path = os.path.join(path, child)
+        if os.path.isdir(child_path):
+            print_directory_contents(child_path)
+        else:
+            print(child_path)
+
+
+print_directory_contents('functions')

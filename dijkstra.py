14b1287aab5833ab456e4cf61a50c1ae57bfadd7
Beth Tenorio <betsky98@gmail.com>
2018-12-18 14:30:43 -0500

    refactor dijkstra code to move nodes and neighbors to init and use a list for paths

--- dijkstra.py
+++ dijkstra.py
@@ -4,7 +4,7 @@ Adapted from: https://dev.to/mxl/dijkstras-algorithm-in-python-algorithms-for-be
 
 Steps:
 1. Create a list of unvisited nodes
-2. Set distance 0 for start node, Inf for the resn
+2. Set distance 0 for start node, Inf for the rest
 3. Find distance of neighbors from current node and save if smaller
 4. Remove node from unvisited list
 5. Stop if destination node, or smallest distance between unvisited nodes is Inf.
@@ -17,29 +17,20 @@ from collections import deque, namedtuple
 inf = float('inf')
 Edge = namedtuple('Edge', 'start, end, cost')
 
-def make_edge(start, end, cost=1):
-    return Edge(start, end, cost)
-
 
 class Graph:
 
     def __init__(self, edges):
-        self.edges = [make_edge(*edge) for edge in edges]
-
-
-    @property
-    def nodes(self):
+        self.edges = [self.make_edge(*edge) for edge in edges]
         all_edges = [[edge.start, edge.end] for edge in self.edges]
-        return [item for sublist in all_edges for item in sublist]
-
-
-    @property
-    def neighbors(self):
-        neighbors = {node: set() for node in self.nodes}
+        self.nodes = [item for sublist in all_edges for item in sublist]
+        self.neighbors = {node: set() for node in self.nodes}
         for edge in self.edges:
-            neighbors[edge.start].add((edge.end, edge.cost))
+            self.neighbors[edge.start].add((edge.end, edge.cost))
 
-        return neighbors
+
+    def make_edge(self, start, end, cost=1):
+        return Edge(start, end, cost)
 
 
     def dijkstra(self, source, dest):
@@ -50,36 +41,44 @@ class Graph:
         ... ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
         ... ("e", "f", 9)])
         >>> graph.dijkstra("a", "e")
-        deque(['a', 'c', 'd', 'e'])
+        ['a', 'c', 'd', 'e']
         """
 
         distances = {node: inf for node in self.nodes}
         path_nodes = {node: None for node in self.nodes}
         distances[source] = 0
-        nodes = self.nodes.copy()
+        visited = self.nodes.copy()
 
-        while nodes:
-            current_node = min(
-                nodes, key=lambda node: distances[node])
-            nodes.remove(current_node)
-            if distances[current_node] == inf:
+        while visited:
+            current = min(visited, key=lambda node: distances[node])
+            visited.remove(current)
+
+            # Stop when the minimum distance is Inf
+            if distances[current] == inf:
                 break
-            for neighbor, cost in self.neighbors[current_node]:
-                alternative_route = distances[current_node] + cost
-                if alternative_route < distances[neighbor]:
-                    distances[neighbor] = alternative_route
-                    path_nodes[neighbor] = current_node
+            for neighbor, cost in self.neighbors[current]:
+                alt_route = distances[current] + cost
+                if alt_route < distances[neighbor]:
+                    distances[neighbor] = alt_route
+                    path_nodes[neighbor] = current
 
-        path, current_node = deque(), dest
-        while path_nodes[current_node] is not None:
-            path.appendleft(current_node)
-            current_node = path_nodes[current_node]
-        if path:
-            path.appendleft(current_node)
+
+        path = []
+        while path_nodes[dest] is not None:
+            path.append(dest)
+            dest = path_nodes[dest]
+
+        path.append(source)
+        path.reverse()
 
         return path
 
 
 if __name__ == '__main__':
     import doctest
+    import logging
+    import sys
+
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    log = logging.info
     doctest.testmod()
